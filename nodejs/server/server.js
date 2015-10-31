a7703b2409d3be863cee32f8fe564ad440f4d0c0
Leelow <leo.lozach@gmail.com>
2015-10-31 18:48:40 +0100

    Update

--- nodejs/server/server.js
+++ nodejs/server/server.js
@@ -1,13 +1,19 @@
 var http = require('http').Server();
 var io = require('socket.io')(http);
 //var daemonIO = require("./lib/daemon.io.js")(io);
-global.wrapper = require("./lib/wrapper.js");
+var wrapper = require("./lib/wrapper.js");
 
 var port = 8080;
 
 //daemonIO.start();
 
-http.listen(wrapper, port, function(){
-    console.log('Server started on *:' + port);
+var test = function(wrapper) {
+
     wrapper.status();
+
+}
+
+http.listen(port, function(){
+    console.log('Server started on *:' + port);
+    test(wrapper);
 });
